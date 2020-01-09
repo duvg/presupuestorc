6cda61d1e3866ec7fad1130a91b7913be2b7a387
Duviel Garcia <duviel7@gmail.com>
2020-01-08 20:59:44 -0500

    Commit Inicial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App container">
+      <header>
+        <h1>Gasto semanal</h1>
+      </header>
+        
+    </div>
+  );
+}
+
+export default App;
